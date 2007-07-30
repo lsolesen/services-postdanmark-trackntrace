290ec767dfbf8e7abc07ac21bc0495da9d7bb996
Lars Olesen <lars@intraface.dk>
2007-07-30 21:33:28 +0000

    Updated docs.

--- Services/PostDanmark.php
+++ Services/PostDanmark.php
@@ -1,23 +1,70 @@
 <?php
 /**
+ * Communicates with PostDanmark Track'n'Trace
+ *
  * http://www.postdanmark.dk/contentfull.dk?content=/cms/da-dk/kontaktos/index.htm&menufile=/cms/da-dk/menufiles/kontaktos.xml&lang=dk
+ *
+ * PHP version 5
+ *
+ * @category  Services
+ * @package   Services_PostDanmark
+ * @author    Lars Olesen <lars@legestue.net>
+ * @copyright 2007 Authors
+ * @license   GPL http://www.opensource.org/licenses/gpl-license.php
+ * @version   @package-version@
+ * @link      http://public.intraface.dk
  */
-
 require_once 'HTTP/Request.php';
 require_once 'HTMLPurifier.php';
 
+/**
+ * Communicates with PostDanmark Track'n'Trace
+ *
+ * http://www.postdanmark.dk/contentfull.dk?content=/cms/da-dk/kontaktos/index.htm&menufile=/cms/da-dk/menufiles/kontaktos.xml&lang=dk
+ *
+ * @category  Services
+ * @package   Services_PostDanmark
+ * @author    Lars Olesen <lars@legestue.net>
+ * @copyright 2007 Authors
+ * @license   GPL http://www.opensource.org/licenses/gpl-license.php
+ * @version   @package-version@
+ * @link      http://public.intraface.dk
+ */
 class Services_PostDanmark
 {
+    /**
+     * @var integer
+     */
     private $number;
 
-    public function __construct($number)
+    /**
+     * @var string
+     */
+    private $url;
+
+    /**
+     * Constructor
+     *
+     * @param integer $number Track'n'Trace number
+     * @param string  $url    Url at PostDanmark
+     *
+     * @return void
+     */
+    public function __construct($number, $url = 'http://www.postdanmark.dk/tracktrace/TrackTracePrint.do?i_stregkode=')
     {
         $this->number = $number;
+        $this->url    = $url;
+
     }
 
+    /**
+     * Request information
+     *
+     * @return object
+     */
     public function request()
     {
-        $req = new HTTP_Request('http://www.postdanmark.dk/tracktrace/TrackTracePrint.do?i_stregkode=' . $this->number);
+        $req = new HTTP_Request($this->url . rawurlencode($this->number));
 
         if (!PEAR::isError($req->sendRequest())) {
             if (!$response = $req->getResponseBody()) {
@@ -29,6 +76,13 @@ class Services_PostDanmark
         throw new Exception('could not perform request');
     }
 
+    /**
+     * Cleans up the response so it can be worked with
+     *
+     * @param string  $response The response from PostDanmark Webservice
+     *
+     * @return string
+     */
     static public function cleanupResponse($response)
     {
         $response = preg_replace("@<script[^>]*?>.*?</script>@si", '', $response);
@@ -40,6 +94,11 @@ class Services_PostDanmark
         return $purifier->purify($response);
     }
 
+    /**
+     * Queries the PostDanmark service
+     *
+     * @return ArrayObject
+     */
     public function query()
     {
         $response = $this->request();
